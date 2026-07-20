308dfe63d89715a774b9529bbfbbc55dc2b1762f
Moomerator46 <97137823+Moomerator46@users.noreply.github.com>
2026-07-20 14:13:04 -0500

    Add fullscreen Tkinter app with context menu

---
+++ EXAMPLE.py
@@ -0,0 +1,25 @@
+from Pymenugui import MenuBar, ContextMenu # Both classes
+import tkinter as tk
+import sys # To close the app
+
+THEME_BG = "#0a0a2e"
+THEME_MENU = "#0c0c4f"
+THEME_ACCENT = "#d000ff"
+THEME_TEXT = "#ffffff"
+MAIN_FONT = ('Arial', 10)
+
+root = tk.Tk()
+root.attributes('-fullscreen', True) # Makes the window fullscreen
+bigframe = tk.Frame(root, bg=THEME_BG) # A frame as big as the window as a widget for the Context Menu
+bigframe.pack(fill=tk.BOTH, expand=True)
+
+contextmenucommands = [
+  ('Test one', lambda: print("Test1 clicked")),
+  ('Test two', lambda: print("Test2 clicked")),
+  ('SEPARATOR', None),
+  ('Test three', lambda print("Test3 clicked"))
+]
+
+ContextMenu(root, bigframe, THEME_MENU, THEME_TEXT, THEME_ACCENT, MAIN_FONT, contextmenucommands) # Injects context menu
+
+menus = []
