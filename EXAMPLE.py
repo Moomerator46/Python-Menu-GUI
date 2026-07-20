d2271953434e5b2a4a471140e7627f5fc3083c4a
Moomerator46 <97137823+Moomerator46@users.noreply.github.com>
2026-07-20 14:20:58 -0500

    Restore context menu and bigframe initialization
    Reintroduce context menu and bigframe setup after removal.

--- EXAMPLE.py
+++ EXAMPLE.py
@@ -10,6 +10,20 @@ MAIN_FONT = ('Arial', 10)
 
 root = tk.Tk()
 root.attributes('-fullscreen', True) # Makes the window fullscreen
+
+menus = [
+  ('File', [
+    ('Save', lambda: print('Save Pressed')),
+    ('Load', lambda: print('Load Pressed')),
+    ('Close', lambda: sys.exit())
+  ]),
+  ('Edit', [
+    ('Select All', lambda: print('SelectAll Pressed'))
+  ])
+]
+
+MenuBar(root, THEME_MENU, THEME_ACCENT, THEME_TEXT, MAIN_FONT, menus) # Injects Menu Bar
+
 bigframe = tk.Frame(root, bg=THEME_BG) # A frame as big as the window as a widget for the Context Menu
 bigframe.pack(fill=tk.BOTH, expand=True)
 bigframe.lower()
@@ -18,21 +32,9 @@ contextmenucommands = [
   ('Test one', lambda: print("Test1 clicked")),
   ('Test two', lambda: print("Test2 clicked")),
   ('SEPARATOR', None),
-  ('Test three', lambda print("Test3 clicked"))
+  ('Test three', lambda: print("Test3 clicked"))
 ]
 
 ContextMenu(root, bigframe, THEME_MENU, THEME_TEXT, THEME_ACCENT, MAIN_FONT, contextmenucommands) # Injects context menu
-
-menus = [
-  ('File', [
-    ('Save', lambda: print('Save Pressed')),
-    ('Load', lambda: print('Load Pressed'))
-  ]),
-  ('Edit', [
-    ('Select All', lambda: print('SelectAll Pressed'))
-  ])
-]
-
-MenuBar(root, THEME_MENU, THEME_ACCENT, THEME_TEXT, MAIN_FONT, menus) # Injects Menu Bar
 
 root.mainloop()
